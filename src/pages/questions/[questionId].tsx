67efd63d7a84cfd83197cf6fe7d98f362f1a85ef
danieiff <sugielff@gmail.com>
2023-05-22 17:45:09 +0900

    preliminary ui for root, 'questions' route

---
+++ src/pages/questions/[questionId].tsx
@@ -0,0 +1,84 @@
+import { Suspense } from "react"
+import { Routes } from "@blitzjs/next"
+import Head from "next/head"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useQuery, useMutation } from "@blitzjs/rpc"
+import { useParam } from "@blitzjs/next"
+
+import Layout from "src/core/layouts/Layout"
+import getQuestion from "src/questions/queries/getQuestion"
+import deleteQuestion from "src/questions/mutations/deleteQuestion"
+import updateChoice from "src/choices/mutations/updateChoice"
+
+export const Question = () => {
+  const router = useRouter()
+  const questionId = useParam("questionId", "number")
+  const [deleteQuestionMutation] = useMutation(deleteQuestion)
+  const [question, { refetch }] = useQuery(getQuestion, { id: questionId })
+
+  const [updateChoiceMutation] = useMutation(updateChoice)
+
+  const handleVote = async (id: number) => {
+    try {
+      await updateChoiceMutation({ id })
+      void refetch()
+    } catch (error) {
+      alert("Error updating choice " + JSON.stringify(error, null, 2))
+    }
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Question {question.text}</title>
+      </Head>
+
+      <div>
+        <h1>Question {question.text}</h1>
+        <ul>
+          {question.choices.map(({ id, text, votes }) => (
+            <li key={id}>
+              {text} - {votes} votes
+              <button onClick={() => handleVote(id)}>Vote</button>
+            </li>
+          ))}
+        </ul>
+
+        <Link href={Routes.EditQuestionPage({ questionId: question.id })}>Edit</Link>
+
+        <button
+          type="button"
+          onClick={async () => {
+            if (window.confirm("This will be deleted")) {
+              await deleteQuestionMutation({ id: question.id })
+              await router.push(Routes.QuestionsPage())
+            }
+          }}
+          style={{ marginLeft: "0.5rem" }}
+        >
+          Delete
+        </button>
+      </div>
+    </>
+  )
+}
+
+const ShowQuestionPage = () => {
+  return (
+    <div>
+      <p>
+        <Link href={Routes.QuestionsPage()}>Questions</Link>
+      </p>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Question />
+      </Suspense>
+    </div>
+  )
+}
+
+ShowQuestionPage.authenticate = true
+ShowQuestionPage.getLayout = (page) => <Layout>{page}</Layout>
+
+export default ShowQuestionPage
